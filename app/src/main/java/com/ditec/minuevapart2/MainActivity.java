593e0240dd1dda37ec4630ad3b437fe1708ee709
Wilson Callisaya <demarktec@gmail.com>
2020-11-06 21:09:39 -0500

    Usando los tags

--- app/src/main/java/com/ditec/minuevapart2/MainActivity.java
+++ app/src/main/java/com/ditec/minuevapart2/MainActivity.java
@@ -126,4 +126,12 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
     public void btnizquierda(View view) {
         Toast.makeText(this, "Se pulso el floating button de la izquierda "+ view.getTag(), Toast.LENGTH_LONG).show();
     }
+
+    public void btntag(View view) {
+        if(view.getTag().equals("fabizquierda"))
+            Toast.makeText(this, "Se pulso el floating button de la izquierda "+ view.getTag(), Toast.LENGTH_LONG).show();
+        else
+            Toast.makeText(this, "Se pulso el floating button de la derecha "+ view.getTag(), Toast.LENGTH_LONG).show();
+
+    }
 }
